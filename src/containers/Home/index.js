666b28303fdb150fd275db34b7bc341718ab269f
Baltazar <balti.kpo@gmail.com>
2022-05-02 12:47:15 -0300

    First commit

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import Searcher from '../../components/Searcher';
-import './styles.css';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPokemon } from "../../actions";
+import { getPokemons } from "../../api/getPokemons";
+import PokemonList from "../../components/PokemonList";
+import Searcher from "../../components/Searcher";
+import "./styles.css";
 
 function Home() {
+  const dispatch = useDispatch();
+  const list = useSelector((state) => state.list);
+
+  useEffect(() => {
+    getPokemons()
+      .then((res) => {
+        dispatch(setPokemon(res.results));
+      })
+      .catch(console.log);
+  }, []);
+
   return (
-    <div className='Home'>
+    <div className="Home">
       <Searcher />
+      <PokemonList pokemons={list} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import App from "./containers/App";
+import "./index.css";
+import { logActions } from "./middlewares";
+import pokemonReducer from "./reducers/pokemonReducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logActions)
+);
+
+const store = createStore(pokemonReducer, composedEnhancers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
